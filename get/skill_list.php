01f01c7dbd9bd9bf806559866451b6c7ca6a58f3
Abhinav Valecha <valecha.abhinav@gmail.com>
2017-03-28 19:22:14 +0530

    Add DB connection and return skill list based on table names

--- get/skill_list.php
+++ get/skill_list.php
@@ -1,4 +1,37 @@
 <?php
-	$skill_list = array("Cook", "Driver");
+
+	$non_skill_tables = array("employee", "employers");
+
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "linking";
+
+	// Create connection
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+	// Check connection
+	if (!$conn) {
+	    die("Connection failed: " . mysqli_connect_error());
+	}
+
+	$sql = "SHOW TABLES";
+	$result = mysqli_query($conn, $sql);
+	mysqli_close($conn);
+
+	$skill_list = array();
+	if (mysqli_num_rows($result) > 0) {
+	    while($row = mysqli_fetch_assoc($result)) {
+	    	$found = FALSE;
+	    	foreach ($non_skill_tables as $table_name) {
+	    		if (preg_match("/$table_name/i", $row["Tables_in_linking"]) == 1) {
+	    			$found = TRUE;
+	    			break;
+	    		}
+	    	}
+	    	if ($found == FALSE) {
+	    		array_push($skill_list, $row["Tables_in_linking"]);
+	    	}
+	    }
+	}
 	print_r(json_encode($skill_list));
 ?>

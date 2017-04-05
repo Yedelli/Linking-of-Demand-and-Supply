65a27f82a392958584d1b04cadcf87a333245dda
Ravi Yadav <yravi7861@gmail.com>
2017-04-05 16:31:24 +0530

    added validations

--- SE/admin_form_edit.php
+++ SE/admin_form_edit.php
@@ -7,6 +7,21 @@
 	$row= mysql_fetch_array ($result);
 ?>
 	<html>
+		<head>
+			<meta charset="utf-8"/>
+		    	<meta name="viewport" content="width=device-width, initial-scale=1">
+		    	<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		
+			<script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.9/jquery.validate.min.js"></script>
+
+			<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+			<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+		       	    
+				
+		       	    <script type="text/javascript" src="valid.js" ></script>
+		</head>
 		<body>
 			<div class="container">				
 				<br>
